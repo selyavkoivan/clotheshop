b9c453303f0ff406701fea52f63024bcb167edbf
sialiaŭkaivan <selyavkosh2@gmail.com>
2022-04-26 15:17:39 +0300

    remove directories

--- src/main/java/by/bsuir/clotheshop/model/service/cloudinary/data/CloudData.java
+++
@@ -1,7 +0,0 @@
-package by.bsuir.clotheshop.model.service.cloudinary.data;
-
-public class CloudData {
-    public static final String CLOUD_NAME = "fanfictionteamoff";
-    public static final String API_KEY = "137398413134971";
-    public static final String API_SECRET = "TZhA_4TcnI9TUke8P-6VCDkJ0SM";
-}
